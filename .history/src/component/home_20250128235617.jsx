422802b9dd6a040fd79c3f0b304543926ee3dcec
Akainu <akainu@localhost>
2025-01-28 23:57:40 +0100

    home

--- .history/src/component/home_20250128235617.jsx
+++ .history/src/component/home_20250128235617.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>hello my name is joel you can contact me for the creation of web apps</h1>
+      <h1></h1>
     </div>
   )
 }
--- .history/src/component/home_20250128235648.jsx
+++ .history/src/component/home_20250128235648.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>hello my name is joel you can contact me for the creation of web apps</h1>
+      <h1>hello my name is joel you can contact me for webapp</h1>
     </div>
   )
 }
