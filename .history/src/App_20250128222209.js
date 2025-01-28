54f759f23c0e5d0b0bbea49929d15a99f85dac81
Akainu <akainu@localhost>
2025-01-28 22:23:13 +0100

    com

--- .history/src/App_20250128222209.js
+++ .history/src/App_20250128222209.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './component/home';
+import 
 
 function App() {
   return (
---
+++ .history/src/component/home_20250128222004.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ome = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ome
